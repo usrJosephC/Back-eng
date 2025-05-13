e80ae90b587165a5f889f7d60b418f597c0080c9
lelepton <leticyabeatryz@hotmail.com>
2025-05-13 18:25:01 -0300

    refactor: rename get_image to get_info and dict to info for clarity

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from spotify_requests.get_image import dict
+from spotify_requests.get_info import info
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify(dict)
+    return jsonify(info)
 
 if __name__ == '__main__':
     app.run(debug=True)
