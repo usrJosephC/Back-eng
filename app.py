40ee20a31882e87b7b6d1555c933c19ebb8a8731
lelepton <leticyammedeiros@gmail.com>
2025-05-23 00:22:13 -0300

    refactor: update Flask app to receive year via POST and return song info dynamically using get_info function

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from spotify_requests.get_info import info
+from spotify_requests.get_info import get_info
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify(info)
+@app.route('/year', methods=['POST'])
+def receive_year():
+    '''receives the year from the user and returns a dictionary with the song'''
+    data = request.get_json()
+    year = data.get('year')
 
+    if not year:
+        return jsonify({'error': 'Year not provided'}), 400
+
+    try:
+        year = int(year)
+    except ValueError:
+        return jsonify({'error': 'Year must be an integer'}), 400
+
+    result = get_info(year)
+    return jsonify(result)
+    
 if __name__ == '__main__':
     app.run(debug=True)
     
