961720b4b6078bdbd8a57f05ec1fe1f383f3d7a8
lelepton <leticyammedeiros@gmail.com>
2025-06-05 20:12:17 -0300

    refactor: turn spotify_auth into function and add sp parameter to play_music for Spotify client

--- spotify_requests/play_music.py
+++ spotify_requests/play_music.py
@@ -1,14 +1,28 @@
-from .spotify_auth import sp
+from .spotify_auth import spotify_auth
 from .get_device_id import get_device_id
 
-def play_music(uris: list, device_id: str = None):
-    device_id = device_id or get_device_id()
-    if not device_id:
-        print("❌ Não foi possível encontrar dispositivo!")
-        return
-
+def play_music(sp, uris: list, device: str):
+    '''play music using the Spotify API'''
     try:
-        sp.start_playback(device_id=device_id, uris=uris)
-        print(f"▶️ Tocando {len(uris)} músicas no dispositivo {device_id[:5]}...")
+        sp.start_playback(device_id=device,
+                          context_uri=None,
+                          uris=uris,
+                          offset=None,
+                          position_ms=None)
+        print("Playing music...")
     except Exception as e:
-        print(f"🚨 Erro ao tocar: {e}")
+        print(f"An error occurred while trying to play music: {e}")
+
+songs = [
+    '0HPD5WQqrq7wPWR7P7Dw1i',
+    '1c8gk2PeTE04A1pIDH9YMk',
+    '60nZcImufyMA1MKQY3dcCH',
+    '50kpGaPAhYJ3sGmk6vplg0'
+]
+
+# calling the necessary functions to authenticate and play music
+sp_auth = spotify_auth()
+uris_list = [f'spotify:track:{track_id}' for track_id in songs]
+device_id = get_device_id(sp_auth)
+
+play_music(sp_auth, uris_list, device_id)
--- spotify_requests/spotify_auth.py
+++ spotify_requests/spotify_auth.py
@@ -5,24 +5,27 @@ from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-load_dotenv()
+def spotify_auth():
+    load_dotenv()
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-redirect_uri = os.getenv('REDIRECT_URI')
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+    redirect_uri = os.getenv('REDIRECT_URI')
 
-if os.path.exists('.cache'):
-    os.remove('.cache')
+    if os.path.exists('.cache'):
+        os.remove('.cache')
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri=redirect_uri,
-                                               scope="playlist-modify-public " \
-                                                    "user-read-playback-state " \
-                                                    "user-modify-playback-state " \
-                                                    "user-library-read " \
-                                                    "user-library-modify " \
-                                                    "user-read-currently-playing " \
-                                                    "user-read-playback-position " \
-                                                    "user-read-recently-played " \
-                                                    "streaming"))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                                client_secret=client_secret,
+                                                redirect_uri=redirect_uri,
+                                                scope="playlist-modify-public " \
+                                                        "user-read-playback-state " \
+                                                        "user-modify-playback-state " \
+                                                        "user-library-read " \
+                                                        "user-library-modify " \
+                                                        "user-read-currently-playing " \
+                                                        "user-read-playback-position " \
+                                                        "user-read-recently-played " \
+                                                        "streaming"))
+    
+    return sp
