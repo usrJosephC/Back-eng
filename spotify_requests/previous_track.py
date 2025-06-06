82783b81b7c561b551af34494ef4344387834cc5
lelepton <leticyammedeiros@gmail.com>
2025-06-05 21:06:08 -0300

    refactor: add sp parameter to previous_track for Spotify client

--- spotify_requests/previous_track.py
+++ spotify_requests/previous_track.py
@@ -5,6 +5,6 @@ def previous_track(sp, device_id: str):
         return
 
     try:
-        sp.previous_track(device_id=device_id)
+        sp.previous_track(device_id)
     except Exception as e:
         print(f"Error while trying to go to the previous track: {e}")
