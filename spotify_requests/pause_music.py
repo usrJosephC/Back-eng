c11d1e29da8095ff2be2fecdee7fab5c5943de5c
lelepton <leticyammedeiros@gmail.com>
2025-06-05 21:03:20 -0300

    refactor: add sp parameter to pause_music  for Spotify client

--- spotify_requests/pause_music.py
+++ spotify_requests/pause_music.py
@@ -1,15 +1,9 @@
-from .spotify_auth import sp
-from .get_device_id import get_device_id
-
-def pause_music(device_id: str):
+def pause_music(sp, device_id: str):
     if not device_id:
-        print("❌ Dispositivo não encontrado!")
+        print("An error occurred: No device ID found.")
         return
 
     try:
         sp.pause_playback(device_id)
-        print(f"⏸️ Música pausada")
     except Exception as e:
-        print(f"🚨 Erro ao pausar: {e}")
-
-pause_music(get_device_id())
+        print(f"An error occurred while trying to pause the music: {e}")
