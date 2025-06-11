0ff599b03e04b548ecee13f48ac49bdfefd69745
lelepton <leticyammedeiros@gmail.com>
2025-06-10 23:00:03 -0300

    fix: handle token refresh and return updated token info in spotify_client

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request, session, redirect, url_for
+from flask import Flask, jsonify, request, session, redirect
 from flask_cors import CORS
 
 from table.table import table
