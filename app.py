a20a608eb55c653dc659e962bd54afcaee4f7ae0
lelepton <leticyammedeiros@gmail.com>
2025-06-12 16:33:53 -0300

    refactor: remove redis and create success route to redirect user after login

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
-from redis import Redis
 from flask import Flask, jsonify, request, session, redirect
-from flask_session import Session
 from flask_cors import CORS
 
 from table.table import table
@@ -22,15 +20,11 @@ CORS(app, origins=['http://localhost:3000', 'https://divebackintime.onrender.com
                     supports_credentials=True)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 app.config.update(
-    SESSION_TYPE='redis',  # use Redis for session storage
-    SESSION_REDIS=Redis.from_url(os.getenv("REDIS_URL")),  # connect to Redis using the URL from environment variable
     SESSION_COOKIE_NAME='spotify_session', # name of the session cookie
     SESSION_COOKIE_HTTPONLY=True, # prevents JavaScript access to the cookie
-    SESSION_COOKIE_SECURE=False,  # true since we use HTTPS, is secure #CHANGED FOR A BIT FOR TESTING
+    SESSION_COOKIE_SECURE=True,  # true since we use HTTPS, is secure
     SESSION_COOKIE_SAMESITE='None'  # since our frontend and backend are on different domains
 )
-
-Session(app)  # initialize the session with the Flask app
 
 sp_oauth = auth_manager()
 
