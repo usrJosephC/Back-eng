20ada61c6c0b675390e0e6cd9a8eed2e42e9ef4f
lelepton <leticyammedeiros@gmail.com>
2025-06-13 09:18:10 -0300

    feat: add /api/ for every route and remove CORS

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
-from flask import Flask, jsonify, request, session, redirect
-from flask_cors import CORS
+from flask import Flask, jsonify, request, session, redirect, send_from_directory
 
 from table.table import table
 
 from spotify_requests.auth_manager import auth_manager
 from spotify_requests.spotify_client import spotify_client
 from spotify_requests.get_info import get_info
-# from spotify_requests.get_device_id import get_device_id
 from spotify_requests.get_song_id import get_song_id
 from spotify_requests.play_music import play_music
 from spotify_requests.previous_track import previous_track
@@ -15,31 +13,34 @@ from spotify_requests.pause_music import pause_music
 from spotify_requests.next_track import next_track
 from spotify_requests.create_playlist import create_playlist
 
-app = Flask(__name__)
-CORS(app, origins='https://divebackintime.onrender.com', supports_credentials=True)
+app = Flask(__name__, static_folder='frontend_build', static_url_path='')
 
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 app.config.update(
-    SESSION_COOKIE_DOMAIN='backend-divebackintime.onrender.com',
     SESSION_COOKIE_NAME='spotify_session', # name of the session cookie
     SESSION_COOKIE_HTTPONLY=True, # prevents JavaScript access to the cookie
     SESSION_COOKIE_SECURE=True,  # true since we use HTTPS, is secure
-    SESSION_COOKIE_SAMESITE='None'  # since our frontend and backend are on different domains
+    SESSION_COOKIE_SAMESITE='Lax'  # prevents CSRF attacks
 )
 
-@app.route('/', methods=['GET'])
-def index():
-    '''returns a message to check if the server is running'''
-    return jsonify({'message': 'Server is running!'}), 200
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_frontend(path):
+    if path.startswith('api/'):
+        return jsonify({'error': 'API endpoint not found'}), 404
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/login', methods=['GET'])
+@app.route('/api/login', methods=['GET'])
 def login():
     '''redirects the user to Spotify's authentication page'''
     sp_oauth = auth_manager()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
-@app.route('/callback', methods=['GET'])
+@app.route('/api/callback', methods=['GET'])
 def callback():
     '''handles the callback from Spotify after user authentication'''
     code = request.args.get('code')
@@ -54,12 +55,7 @@ def callback():
     except Exception as e:
         return jsonify({'error': f'An error occurred during authentication: {e}'}), 500
     
-# @app.route('/success', methods=['GET'])
-# def success():
-#     '''returns a success message after successful authentication'''
-#     return jsonify({'message': 'Authentication successful! You can now close this tab.'}), 200
-    
-@app.route('/token', methods=['GET'])
+@app.route('/api/token', methods=['GET'])
 def send_token():
     '''returns the access token to the frontend if it exists in the session'''
     token_info = session.get('token_info', None)
@@ -68,7 +64,7 @@ def send_token():
     else:
         return jsonify({'error': 'No valid token found.'}), 401
 
-@app.route('/device', methods=['POST'])
+@app.route('/api/device', methods=['POST'])
 def receive_device_id():
     '''receives the device ID from the frontend and stores it for later use'''
     data = request.get_json()
@@ -81,7 +77,7 @@ def receive_device_id():
     session['device_id'] = device_id
     return jsonify({'message': 'Device ID stored successfully'}), 200
 
-@app.route('/year', methods=['GET'])
+@app.route('/api/year', methods=['GET'])
 def receive_year():
     '''receives the year from the frontend and returns the songs from that year until the end of the table'''
     year = request.args.get('year')
@@ -111,7 +107,7 @@ def receive_year():
 
     return jsonify(result)
 
-@app.route('/play', methods=['GET'])
+@app.route('/api/play', methods=['GET'])
 def play():
     '''plays the songs starting from the year provided by the user'''
 
@@ -136,7 +132,7 @@ def play():
     except Exception as e:
         return jsonify({'error': f'An error occurred while trying to play music: {e}'}), 500
     
-@app.route('/previous', methods=['GET'])
+@app.route('/api/previous', methods=['GET'])
 def previous():
     '''skips to the previous song in the list'''
 
@@ -157,7 +153,7 @@ def previous():
     except Exception as e:
         return jsonify({'error': f'An error occurred while trying to skip to previous track: {e}'}), 500
 
-@app.route('/pause', methods=['GET'])
+@app.route('/api/pause', methods=['GET'])
 def pause():
     '''pauses the currently playing song'''
 
@@ -178,7 +174,7 @@ def pause():
     except Exception as e:
         return jsonify({'error': f'An error occurred while trying to pause playback: {e}'}), 500
     
-@app.route('/next', methods=['GET'])
+@app.route('/api/next', methods=['GET'])
 def play_next():
     '''skips to the next song in the list'''
     
@@ -199,7 +195,7 @@ def play_next():
     except Exception as e:
         return jsonify({'error': f'An error occurred while trying to skip to next track: {e}'}), 500
 
-@app.route('/playlist', methods=['POST'])
+@app.route('/api/playlist', methods=['POST'])
 def make_playlist():
     '''receives the years of songs from the frontend and creates a playlist with those songs'''
     data = request.get_json()
