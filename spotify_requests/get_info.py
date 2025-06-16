e72563f6bc06eeb11ee23bd8c49394713861e59c
lelepton <leticyammedeiros@gmail.com>
2025-06-16 03:32:37 -0300

    refactor: change parameter name to not overwrite it by the loop name

--- spotify_requests/get_info.py
+++ spotify_requests/get_info.py
@@ -1,17 +1,17 @@
 from table.table import filter_by_year
 
-def get_info(year: int):
+def get_info(chosen_year: int):
     '''Returns a dictionary with the song information starting from the year
     provided by the user, using data from the local CSV table.'''
 
     info = {}
-    filtered_table = filter_by_year(year)
+    filtered_table = filter_by_year(chosen_year)
     year_list = filtered_table['YEAR']
     song_list = filtered_table['SONG']
     artist_list = filtered_table['ARTIST']
     song_img_list = filtered_table['SONG_IMG']
     track_duration_list = filtered_table['TRACK_DURATION']
-    
+
     for i, year in enumerate(year_list):
         info[int(year)] = {
             'song_name': song_list[i],
